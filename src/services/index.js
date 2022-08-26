f237aedcea1f165aa9e70beb0060e6af99d19401
zClut <juandanielpadilla0@gmail.com>
2022-08-26 19:15:37 +0100

    :pushpin: | Adding services to get data

---
+++ src/services/index.js
@@ -0,0 +1,17 @@
+import client from "../axios"
+
+const channels = [
+    { name: "alexby11", platform: "twitch" },
+    { name: "bystaxx", platform: "twitch" },
+    { name: "illojuan", platform: "twitch" },
+    { name: "lolitofdez", platform: "twitch" },
+    { name: "luzu", platform: "twitch" },
+    { name: "mangel", platform: "twitch" },
+    { name: "quackity", platform: "twitch" },
+    { name: "rubius", platform: "twitch" },
+]
+
+export const getData = async () => {
+    const { data } = await client.post("channels/", channels);
+    return data
+}
