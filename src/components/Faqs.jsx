28686675abb9e3405d3ddfee4c082b0460023aa9
zClut <juandanielpadilla0@gmail.com>
2022-08-26 19:17:05 +0100

    :pushpin: | Component to FAQs

---
+++ src/components/Faqs.jsx
@@ -0,0 +1,15 @@
+const Faqs = () => {
+    return (
+        <>
+            <section className="section is-large">
+                <div className="container">
+                    <div className="section-header">
+                        <h1>Preguntas Frecuentes</h1>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+ 
+export default Faqs;
