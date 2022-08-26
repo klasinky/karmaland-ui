08e544ac6a8705b09b5888f3c87e8e101ff1a06e
zClut <juandanielpadilla0@gmail.com>
2022-08-26 19:31:35 +0100

    :pushpin: | Finish faqs component and css

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -1,15 +1,31 @@
+const data = [
+    {
+        question: "Which streams do I need to watch to get drops?",
+        answer: "However, streamer-specific drops will require you to watch a specific streamer in order to get their drop. All participating streamers are listed above and all active campaigns and their conditions can always be seen on"
+    }
+]
+
 const Faqs = () => {
     return (
         <>
             <section className="section is-large">
                 <div className="container">
                     <div className="section-header">
-                        <h1>Preguntas Frecuentes</h1>
+                        <h1>Faqs</h1>
+                    </div>
+
+                    <div className="section-body">
+                        {data.map(({ question, answer }) => (
+                            <div className="faq-question">
+                                <h2>{question}</h2>
+                                <p>{answer}</p>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </section>
         </>
     );
 }
- 
+
 export default Faqs;
