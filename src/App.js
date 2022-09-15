624bad213ed5761b071879b7eb266d1f154f7f33
klasinky <manugonzl25@gmail.com>
2022-09-15 17:46:17 +0100

    :pushpin: | navbar wip

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { karmaland, tortillaland } from './data';
 import { karmalandLogo, tortillalandLogo } from './utils';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/"  />
+        <Route path="/" exact element={<Home/>}/>
         <Route
           path="karmaland"
           element={
